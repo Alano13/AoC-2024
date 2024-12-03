8e70d16a479bb0f896a6c389d1c1bd3c09d4e253
Vadim_Nesterov <alan@xaker.ru>
2024-12-04 00:05:38 +0300

    added some helpers

--- src/utils.clj
+++ src/utils.clj
@@ -10,7 +10,7 @@
   (let [actual-test-value (get-result (str "src/day" day-number "/test_input.txt"))]
     (if (= actual-test-value expected-test-value)
       (println (str green-output-color "Right answer!" reset-output-color))
-      (println (str red-output-color "Wrong answer Expected: " expected-test-value ". Received: " actual-test-value " " reset-output-color)))))
+      (println (str red-output-color "Wrong answer! Expected: " expected-test-value ". Received: " actual-test-value " " reset-output-color)))))
 
 (defn evaluate-results
   [part1, part1-test-expected-result, part2, part2-test-expected-result, day-number]
@@ -24,3 +24,6 @@
   (println "Result: ")
   (println (str "Part 1: " (part1 (str "src/day" day-number "/input.txt"))))
   (println (str "Part 2: " (part2 (str "src/day" day-number "/input.txt")))))
+
+(defn execute-test-part [part, day-number] (part (str "src/day" day-number "/test_input.txt")))
+(defn execute-part [part, day-number] (part (str "src/day" day-number "/input.txt")))
