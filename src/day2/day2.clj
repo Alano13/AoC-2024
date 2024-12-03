dd2f5ca8d3832366fdb4853b0334a0505a2f892c
Vadim_Nesterov <alan@xaker.ru>
2024-12-03 22:29:18 +0300

    little improvements

--- src/day2/day2.clj
+++ src/day2/day2.clj
@@ -15,8 +15,13 @@
   [report]
   (let [diffs (map #(apply - %) (partition 2 1 report))]
     (or
-     (every? #(and (>= % 1) (<= % 3)) diffs) 
-     (every? #(and (>= % -3) (<= % -1)) diffs))))
+     (every? #(<= 1 % 3) diffs) 
+     (every? #(<= -3 % -1) diffs))))
+
+(defn part1
+  [path]
+  (count (filter is-safe (read-reports path))))
+
 
 (defn skip-ith [collection i]
   (->>
@@ -30,13 +35,9 @@
    (map #(skip-ith report %))
    (some is-safe)))
 
-
-(defn part1
-  [path]
-  (count (filter is-safe (read-reports path))))
-
 (defn part2
   [path]
   (count (filter is-almost-safe (read-reports path))))
+
 
 (evaluate_results part1 2 part2 4 2)
